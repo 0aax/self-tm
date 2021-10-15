741faf9399b423ef66f0ae4cd85dfe0a342de029
Audrey Xie <audrey.hy.xie@gmail.com>
2021-10-15 10:19:28 -0400

    finally correct

--- quine.py
+++ quine.py
@@ -1,20 +1,18 @@
 def self(qt, tb):
     _self_ = r"""
-def self(qt, tb): 
+def self(qt, tb):
     _self_ = r
-    print(_self_[:34] + qt + qt + qt)
-    print(_self_[1:264])
+    print(_self_[:33] + qt + qt + qt)
+    print(_self_[1:263])
     print(tb + qt + qt + qt)
-    print(_self_[35:251])
-    print(_self_[252:257] + repr(_self_[257]) + _self_[258] + repr(_self_[259:263]) + _self_[263])
+    print(_self_[34:250])
+    print(_self_[251:256] + repr(_self_[256]) + _self_[257] + repr(_self_[258:262]) + _self_[262])
 self(",    )
     """
-    print(_self_[:34] + qt + qt + qt)
-    print(_self_[1:264])
+    print(_self_[:33] + qt + qt + qt)
+    print(_self_[1:263])
     print(tb + qt + qt + qt)
-    print(_self_[35:251])
-    print(_self_[252:257] + repr(_self_[257]) + _self_[258] + repr(_self_[259:263]) + _self_[263])
+    print(_self_[34:250])
+    print(_self_[251:256] + repr(_self_[256]) + _self_[257] + repr(_self_[258:262]) + _self_[262])
 self('"','    ')
-
-
 
