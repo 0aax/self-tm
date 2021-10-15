bddc04eff64c38cd0de235920ea1879bd54f9188
Audrey Xie <audrey.hy.xie@gmail.com>
2021-10-15 09:49:12 -0400

    working quine

---
+++ quine.py
@@ -0,0 +1,22 @@
+def self(qt, tb):
+    _self_ = r"""
+def self(): 
+    _self_ = r
+    print(_self_[:37] + _self_[37:])
+    self('\"','    ')
+    print(_self_[:28] + qt + qt + qt)
+    print(_self_[1:304])
+    print(tb + qt + qt + qt)
+    print(_self_[88:304])
+    print(_self_[305:310] +  repr(_self_[310]) + _self_[311], repr(_self_[312:316]) + _self_[316])
+self(",    )
+    """
+    print(_self_[:28] + qt + qt + qt)
+    print(_self_[1:304])
+    print(tb + qt + qt + qt)
+    print(_self_[88:304])
+    print(_self_[305:310] +  repr(_self_[310]) + _self_[311], repr(_self_[312:316]) + _self_[316])
+self('"','    ')
+
+
+
