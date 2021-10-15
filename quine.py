63852878418f8d1da96d222efbbbc678ed063622
Audrey Xie <audrey.hy.xie@gmail.com>
2021-10-15 10:13:55 -0400

    update indices to match

--- quine.py
+++ quine.py
@@ -2,8 +2,8 @@ def self(qt, tb):
     _self_ = r"""
 def self(qt, tb): 
     _self_ = r
-    print(_self_[:28] + qt + qt + qt)
-    print(_self_[1:258])
+    print(_self_[:34] + qt + qt + qt)
+    print(_self_[1:264])
     print(tb + qt + qt + qt)
     print(_self_[35:251])
     print(_self_[252:257] + repr(_self_[257]) + _self_[258] + repr(_self_[259:263]) + _self_[263])
